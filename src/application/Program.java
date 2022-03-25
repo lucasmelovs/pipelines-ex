421930cfdfa136b14831c2bdd15ffc00e0ef67c3
lucasmelovs <lucasmelovs@gmail.com>
2022-03-25 14:09:37 -0300

    Projeto finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -13,24 +14,24 @@ import entities.Employee;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		String path = "c:\\temp\\pipelineEmployee.txt";
+		System.out.print("Enter full file path: ");
+		String path = sc.nextLine();
 		
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-			
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
 			List<Employee> list = new ArrayList<>();
 			
 			String line = br.readLine();
-			while(list != null) {
+			while (line != null) {
 				String[] fields = line.split(",");
 				list.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
 				line = br.readLine();
 			}
-			
+
 			System.out.print("Enter salary: ");
 			double salary = sc.nextDouble();
 			
@@ -39,20 +40,19 @@ public class Program {
 					.map(x -> x.getEmail())
 					.sorted()
 					.collect(Collectors.toList());
-			
+
 			System.out.println("Email of people whose salary is more than " + String.format("%.2f", salary) + ":");
 			emails.forEach(System.out::println);
 			
-			
 			double sum = list.stream()
-					.filter(x -> x.getName().charAt(0)=='M')
+					.filter(x -> x.getName().charAt(0) == 'M')
 					.map(x -> x.getSalary())
-					.reduce(0.0, (x, y) -> x+y);
-			System.out.println(sum);
+					.reduce(0.0, (x, y) -> x + y);
 			
+			System.out.println("Sum of salary from people whose name starts with 'M': " + String.format("%.2f", sum));
 			
-		}catch (Exception e) {
-			System.out.println();
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
 		sc.close();
 	}
